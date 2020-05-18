fd97ed9ada90377cb887463afacfb282e9986cf1
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-05-18 18:48:23 +0100

    stage building and changing

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -5,17 +5,18 @@ using UnityEngine.SceneManagement;
 public class LevelScript : MonoBehaviour
 {
     public List<GameObject> Stages;
-    public static LevelScript levelInstance;
+    
     public float orbitDistance = 100;
-    public const int orbits = 5;
+    public const int orbits = 4;
 
-    [SerializeField]private int currentStage = 1;
+    [SerializeField]private int currentStage = 0;
     [SerializeField] private GameObject currentStageObjects;
 
     [Tooltip("Pls make sure you drag a scene asset into here this is beyond scuffed")]
     [SerializeField] private Object victoryScene;
     [SerializeField] private Object defeatScene;
 
+    public static LevelScript levelInstance;
     // Start is called before the first frame update
     void Awake()
     {
@@ -26,14 +27,14 @@ public class LevelScript : MonoBehaviour
         else
             levelInstance = this;
 
-        //LoadStage(currentStage);
+        LoadStage(currentStage);
 
     }
 
     public void WinStage()
     {
         if (currentStage < Stages.Count)
-            LoadStage(currentStage++);
+            LoadStage(++currentStage);
         else
             // Doesnt work for some reason
             SceneManager.LoadScene(victoryScene.name);
@@ -47,7 +48,8 @@ public class LevelScript : MonoBehaviour
     private void LoadStage(int stage)
     {
         Destroy(currentStageObjects);
-        Instantiate(Stages[stage - 1]);
+        currentStageObjects = Instantiate(Stages[stage - 1]);
+        Debug.Log("loading stage: " + stage);
 
     }
 
