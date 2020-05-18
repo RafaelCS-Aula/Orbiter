8a5eac4e4a2d6b4adf5788f49eff1ab62986484e
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-05-18 05:01:38 +0100

    Orbit checker checks the next orbit for objects infront of player

---
+++ Assets/Scripts/OrbitCheckerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCheckerScript : MonoBehaviour
+{
+    public bool isObstructed;
+    public GameObject obstructor;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isObstructed = true;
+        obstructor = collision.gameObject;
+        Debug.Log("bonk");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isObstructed = false;
+    }
+
+    
+}
