a1a8664f84a3395c9a10ff6bc758b300aff32eea
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-05-18 05:04:04 +0100

    Jumping forward in orbits and life counting, calling win and loss states

---
+++ Assets/Scripts/Orbiters/PlayerOrbiter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOrbiter : OrbiterBase
+{
+    [Header("Player Specific Variables")]
+    public int maxLife = 3;
+    public int life;
+
+    public float orbitJumpCooldown = 0.5f;
+
+    public float orbitCooldownCounter;
+
+    public GameObject forwardChecker;
+
+    [HideInInspector]
+    public OrbitCheckerScript forwardOrbiter;
+
+   
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Setup();
+
+        life = maxLife;
+        orbitCooldownCounter = 0;
+        GameObject instantiated = Instantiate(forwardChecker, transform.position + transform.up * LevelScript.levelInstance.orbitDistance, transform.rotation,this.transform);
+        forwardOrbiter = instantiated.GetComponent<OrbitCheckerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        orbitCooldownCounter += Time.deltaTime;
+
+        transform.RotateAround(Vector3.zero, Vector3.forward, Input.GetAxis("Horizontal") * orbitSpeed * Time.deltaTime);
+
+        // The collision detection isnt working fukfapsodjfmlaksd
+        if (Input.GetAxis("Vertical") > 0 && orbitCooldownCounter > orbitJumpCooldown)
+        {
+            if (forwardOrbiter.isObstructed)
+            {
+
+
+
+
+            }
+            else
+            {
+                transform.position += transform.up * LevelScript.levelInstance.orbitDistance;
+                orbitCooldownCounter = 0;
+                currentOrbit--;
+
+
+            }
+
+
+        }
+
+        if (currentOrbit == 0)
+            LevelScript.levelInstance.WinStage();
+        if (life <= 0)
+        {
+            LevelScript.levelInstance.LoseStage();
+        }
+
+    }
+}
